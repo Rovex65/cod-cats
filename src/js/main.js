522904e673ebebfc44db7744dfb5b5c20f61b870
VadiDem <demochko.vadim@gmail.com>
2024-02-29 18:45:07 +0200

    support & modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
-console.log('hello world');
+const menuModal = document.getElementById("menuModal");
+const openMenuBtn = document.getElementById("openMenuBtn");
+
+
+const closeMenuBtn = document.querySelector("#menuModal .x-close");
+
+
+openMenuBtn.addEventListener("click", function() {
+  menuModal.style.display = "block"; 
+});
+
+const backgroundToggle = document.getElementById("backgroundToggle");
+const modalContent = document.querySelector(".modal-content");
+
+backgroundToggle.addEventListener("change", function() {
+  if (backgroundToggle.checked) {
+    
+    modalContent.style.backgroundColor = "#000"; 
+  } else {
+    modalContent.style.backgroundColor = "#FFF"; 
+  }
+});
+
+closeMenuBtn.addEventListener("click", function() {
+  menuModal.style.display = "none"; 
+});
+
+
+window.addEventListener("click", function(event) {
+  if (event.target == menuModal) {
+    menuModal.style.display = "none"; 
+  }
+});
