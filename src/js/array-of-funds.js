63812e51194f339f9e6f00a691e9648c7c170a8c
nuridze <113994431+nuridze@users.noreply.github.com>
2024-02-29 19:28:03 +0100

    array

---
+++ src/js/array-of-funds.js
@@ -0,0 +1,76 @@
+const save = require('../img/save.png');
+const save2 = require('../img/save@2x.png');
+const hope = require('../img/hope.png');
+const hope2 = require('../img/hope@2x.png');
+const banc = require('../img/24u.png');
+const banc2 = require('../img/24u2x.png');
+const medical = require('../img/medical.png');
+const medical2 = require('../img/medical@2x.png');
+const sans = require('../img/sans.png');
+const sans2 = require('../img/sans@2x.png');
+const razom = require('../img/razom.png');
+const razom2 = require('../img/razom@2x.png');
+const hunger = require('../img/hunger.png');
+const hunger2 = require('../img/hunger@2x.png');
+const vision = require('../img/vision.png');
+const vision2 = require('../img/vision@2x.png');
+const prytula = require('../img/prytula.png');
+const prytula2 = require('../img/prytula@2x.png.');
+const fonds = [
+  {
+    title: 'Save the Children',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: `${save}`,
+    img2x: `${save2}`,
+  },
+  {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    img: `${hope}`,
+    img2x: `${hope2}`,
+  },
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    img: `${banc}`,
+    img2x: `${banc2}`,
+  },
+  {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    img: `${medical}`,
+    img2x: `${medical2}`,
+  },
+  {
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+    img: `${sans}`,
+    img2x: `${sans2}`,
+  },
+  {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+    img: `${razom}`,
+    img2x: `${razom2}`,
+  },
+  {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: `${hunger}`,
+    img2x: `${hunger2}`,
+  },
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: `${vision}`,
+    img2x: `${vision2}`,
+  },
+  {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+    img: `${prytula}`,
+    img2x: `${prytula2}`,
+  },
+];
+
+export { fonds };
