1d15b6598de6a869d1a33b229e0b9282abcdc06b
VadiDem <demochko.vadim@gmail.com>
2024-03-03 18:33:37 +0200

    del hidden on modal-menu

--- src/js/modal-menu.js
+++ src/js/modal-menu.js
@@ -1,10 +1,10 @@
 const burgerMenuButton = document.querySelector('.header-burger-menu');
 const modalMenu = document.querySelector('.header-modal-menu');
+const burgerIcon = document.querySelector('.header-burger-menu svg');
 
 burgerMenuButton.addEventListener('click', function() {
   modalMenu.classList.toggle('active');
 
-const burgerIcon = document.querySelector('.header-burger-menu svg');
   if (modalMenu.classList.contains('active')) {
     burgerIcon.innerHTML = `
       <use href="../img/icons.svg#icon-close"></use>
@@ -14,4 +14,8 @@ const burgerIcon = document.querySelector('.header-burger-menu svg');
       <use href="../img/icons.svg#icon-menu"></use>
     `;
   }
+});
+
+burgerIcon.addEventListener('click', function() {
+  modalMenu.classList.remove('hidden');
 });
