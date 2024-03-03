ea299e853b5f466366baa35955210f14fb2d1335
Rovex65 <rovex65@gmail.com>
2024-03-03 15:45:39 +0200

    fix modal pop up

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,4 +1,4 @@
-import { BookApi } from  './book-api'; 
+import { BookApi } from './book-api';
 
 const bookApi = new BookApi();
 const categoriesList = document.querySelector('.sidebar-categories-name');
@@ -6,20 +6,21 @@ const scrollingLine = document.querySelector('.scrolling-line');
 
 async function renderCategories() {
   try {
-        const categoryList = await bookApi.getCategoryList();
-      
-      categoriesList.innerHTML = categoryList.map(category =>
-          templateNameCategory(category))
-            .join('');
-            
-        const categoryItems = document.querySelectorAll('.sb-category-item');
-        categoryItems.forEach(item => {
-      item.addEventListener('click', async () => {
-          const categoryName = item.querySelector('a').textContent;
-          const books = await bookApi.getBooksByCategory(categoryName);
-          console.log(`Books in category ${categoryName}:`, books);
-      });
-    });
+    const categoryList = await bookApi.getCategoryList();
+
+    categoriesList.insertAdjacentHTML(
+      'beforeend',
+      categoryList.map(category => templateNameCategory(category)).join('')
+    );
+
+    //     const categoryItems = document.querySelectorAll('.sb-category-item');
+    //     categoryItems.forEach(item => {
+    //   item.addEventListener('click', async () => {
+    //       const categoryName = item.querySelector('a').textContent;
+    //       const books = await bookApi.getBooksByCategory(categoryName);
+    //       console.log(`Books in category ${categoryName}:`, books);
+    //   });
+    // });
   } catch (error) {
     console.error('Error fetching category list:', error.message);
   }
