ecfea948914928148443609851d91b94ea1d7f00
Rovex65 <107561862+Rovex65@users.noreply.github.com>
2024-03-03 11:03:07 +0000

    Deploying to gh-pages from @ Rovex65/cod-cats@02e9019ab6e022728b0326788cae114b0b270df3 🚀

---
+++ assets/template-functions-dffd59fd.js
@@ -0,0 +1,38 @@
+(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))r(o);new MutationObserver(o=>{for(const t of o)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&r(l)}).observe(document,{childList:!0,subtree:!0});function i(o){const t={};return o.integrity&&(t.integrity=o.integrity),o.referrerPolicy&&(t.referrerPolicy=o.referrerPolicy),o.crossOrigin==="use-credentials"?t.credentials="include":o.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(o){if(o.ep)return;o.ep=!0;const t=i(o);fetch(o.href,t)}})();function a({author:e,book_image:s,title:i,_id:r}){return`<li class="book-item" data-book-id="${r}">
+              <div class="book-cover" data-js-book>
+                  <img src="${s}" alt="book cover" />
+                  <div class="quick-view">Quick view</div>
+              </div>
+              <div class="book-info">
+                  <p class="book-title">${i}</p>
+                  <p class="book-author">${e}</p>
+              </div>
+          </li>`}function c(e){const{book_image:s,title:i,list_name:r,description:o,author:t,buy_links:l,_id:d}=e;return`<div class="shop-list-book-card">
+  <img src="${s}" class="shop-list-book-image">
+  <div class="shop-list-book-info">
+      <div class="shop-list-card-header">
+          <div class="shop-list-book-title-container">
+              <p class="shop-list-book-title">${i}</p>
+              <p class="shop-list-book-genre">${r}</p>
+          </div>
+          <button type='button' class="delete-icon">
+              <svg class="svg-icon" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                  <path d="M6.75 2.25H11.25M2.25 4.5H15.75M14.25 4.5L13.724 12.3895C13.6451 13.5732 13.6057 14.165 13.35 14.6138C13.1249 15.0088 12.7854 15.3265 12.3762 15.5248C11.9115 15.75 11.3183 15.75 10.132 15.75H7.86799C6.68168 15.75 6.08852 15.75 5.62375 15.5248C5.21457 15.3265 4.87507 15.0088 4.64999 14.6138C4.39433 14.165 4.35488 13.5732 4.27596 12.3895L3.75 4.5M7.5 7.875V11.625M10.5 7.875V11.625" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+          </button>
+      </div>
+      <p class="shop-list-book-description">${o}</p>
+      <div class="shop-list-product-footer">
+          <p class="shop-list-book-author">${t}</p>
+          <div class="shop-list-book-stores">
+              <a href="${l.find(n=>n.name==="Amazon").url}" target="_blank"><img src="./img/amazon-2x.png" class="shop-list-amazon"></a>
+              <a href="${l.find(n=>n.name==="Apple Books").url}" target="_blank"><img src="./img/book-2x.png" class="shop-list-apple"></a>
+          </div>
+      </div>
+  </div>
+</div>`}function p({list_name:e,books:s}){console.log(e,s);const i=s.map(a).join("");return`<section class="books-category">
+              <h3 class="category-title">${e}</h3>
+              <ul class="books-list">${i}</ul>
+              <button type="button" class="button" data-js-category="${e}">See more</button>
+          </section>`}function u(e){return e.map(p).join("")}function f(e){return e.map(c).join()}export{f as a,u as t};
+//# sourceMappingURL=template-functions-dffd59fd.js.map
--- commonHelpers.js
+++ commonHelpers.js
@@ -1,15 +1,2 @@
-import"./assets/styles-0c903ff6.js";import{a as e}from"./assets/vendor-0cb09735.js";function r({author:o,book_image:t,title:s,_id:a}){return`<li class="book-item" data-book-id="${a}">
-              <div class="book-cover" data-js-book>
-                  <img src="${t}" alt="book cover" />
-                  <div class="quick-view">Quick view</div>
-              </div>
-              <div class="book-info">
-                  <p class="book-title">${s}</p>
-                  <p class="book-author">${o}</p>
-              </div>
-          </li>`}function n({list_name:o,books:t}){console.log(o,t);const s=t.map(r).join("");return`<section class="books-category">
-              <h3 class="category-title">${o}</h3>
-              <ul class="books-list">${s}</ul>
-              <button type="button" class="button" data-js-category="${o}">See more</button>
-          </section>`}function i(o){return o.map(n).join("")}class c{constructor(){this.BASE_URL="https://books-backend.p.goit.global/books/",e.defaults.baseURL=this.BASE_URL}async getCategoryList(){return(await e.get("category-list")).data}async getTopBooks(){return(await e.get("top-books")).data}async getBooksByCategory(t){const s=new URLSearchParams({category:t});return(await e.get("category",{params:s})).data}async getBookById(t){return(await e.get(`${t}`)).data}}const l=new c,k=document.querySelector(".books-wrapper");function g(o,t){const s=i(t);o.insertAdjacentHTML("beforeend",s)}async function p(){try{const o=await l.getTopBooks();console.log(o),g(k,o)}catch(o){console.log(o)}}p();const u=new c;async function b(o){const t=await u.getBooksByCategory(o);console.log(t),localStorage.setItem("saved-books",JSON.stringify(t))}b("Audio Fiction");
+import{t as r}from"./assets/template-functions-dffd59fd.js";import{a as s}from"./assets/vendor-0cb09735.js";class a{constructor(){this.BASE_URL="https://books-backend.p.goit.global/books/",s.defaults.baseURL=this.BASE_URL}async getCategoryList(){return(await s.get("category-list")).data}async getTopBooks(){return(await s.get("top-books")).data}async getBooksByCategory(o){const e=new URLSearchParams({category:o});return(await s.get("category",{params:e})).data}async getBookById(o){return(await s.get(`${o}`)).data}}const n=new a,c=document.querySelector(".books-wrapper");function i(t,o){const e=r(o);t.insertAdjacentHTML("beforeend",e)}async function g(){try{const t=await n.getTopBooks();console.log(t),i(c,t)}catch(t){console.log(t)}}g();const k=new a;async function p(t){const o=await k.getBooksByCategory(t);console.log(o),localStorage.setItem("saved-books",JSON.stringify(o))}p("Audio Fiction");
 //# sourceMappingURL=commonHelpers.js.map
