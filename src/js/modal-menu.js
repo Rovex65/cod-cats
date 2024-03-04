a234a1ee340668f03b48c41315434dfcb8d05136
VadiDem <demochko.vadim@gmail.com>
2024-03-04 08:15:03 +0200

    fix style

--- src/js/modal-menu.js
+++ src/js/modal-menu.js
@@ -1,21 +1,44 @@
-const burgerMenuButton = document.querySelector('.header-burger-menu');
-const modalMenu = document.querySelector('.header-modal-menu');
-const burgerIcon = document.querySelector('.header-burger-menu svg');
+export function initBurgerMenu() {
+  const burgerToggle = document.getElementById('burgerToggle');
+  const burgerMenu = document.getElementById('burgerMenu');
+  const header = document.querySelector('header');
 
-burgerMenuButton.addEventListener('click', function() {
-  modalMenu.classList.toggle('active');
+  burgerToggle.addEventListener('click', function () {
+    burgerMenu.classList.toggle('burger-menu-show');
 
-  if (modalMenu.classList.contains('active')) {
-    burgerIcon.innerHTML = `
-      <use href="../img/icons.svg#icon-close"></use>
-    `;
-  } else {
-    burgerIcon.innerHTML = `
-      <use href="../img/icons.svg#icon-menu"></use>
-    `;
+    if (burgerMenu.classList.contains('burger-menu-show')) {
+      burgerToggle.innerHTML = `
+        <svg width="18" height="18" stroke="#000">
+          <use href="../img/icons.svg#icon-close"></use>
+        </svg>`;
+      header.classList.add('overflow-hidden');
+      document.body.classList.add('burger-menu-show'); 
+    } else {
+      burgerToggle.innerHTML = `
+        <svg width="28" height="28">
+          <use href="../img/icons.svg#icon-menu"></use>
+        </svg>`;
+      header.classList.remove('overflow-hidden');
+      document.body.classList.remove('burger-menu-show'); 
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  initBurgerMenu();
+});
+
+const home = document.getElementById('home');
+const shoppingList = document.getElementById('shopping-list');
+
+home.classList.add('active');
+
+const handleClick = event => {
+  if (event.target === home) {
+    home.classList.add('active');
+    shoppingList.classList.remove('active');
+  } else if (event.target === shoppingList) {
+    home.classList.remove('active');
+    shoppingList.classList.add('active');
   }
-});
-
-burgerIcon.addEventListener('click', function() {
-  modalMenu.classList.remove('hidden');
-});
+};
--- src/main.js
+++ src/main.js
@@ -5,3 +5,4 @@ import './js/save-book-local.js';
 import './js/switch-theme.js';
 import './js/categories.js';
 import './js/pop-up.js';
+import './js/modal-menu.js';
