2b2a1479f0926db938a17689464b8ba2f9975669
Aileneboro <a.borovikova89@gmail.com>
2024-03-04 14:18:28 +0200

    header active nav links

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,16 @@
 const activePage = window.location.pathname;
-const navLink = document.querySelectorAll('.header-nav-link');
-navLink.forEach(link => {
-  if (link.href.includes(`${activePage}`)) {
+const navLinks = document.querySelectorAll('.header-nav-link');
+
+let foundActiveLink = false;
+
+navLinks.forEach(link => {
+  const href = link.getAttribute('href');
+  if (href.endsWith(activePage)) {
     link.classList.add('nav-link-active');
+    foundActiveLink = true;
   }
 });
+
+if (!foundActiveLink) {
+  navLinks[0].classList.add('nav-link-active');
+}
