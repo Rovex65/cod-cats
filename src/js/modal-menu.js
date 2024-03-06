852fbec81c76f373b178ace9b8a0a53ba02606ff
Rovex65 <rovex65@gmail.com>
2024-03-07 01:52:14 +0200

    modal menu fix

--- src/js/modal-menu.js
+++ src/js/modal-menu.js
@@ -1,3 +1,6 @@
+import iconClose from '../img/icons.svg#icon-close'
+import iconMenu from '../img/icons.svg#icon-menu'
+
 export function initBurgerMenu() {
   const burgerToggle = document.getElementById('burgerToggle');
   const burgerMenu = document.getElementById('burgerMenu');
@@ -9,17 +12,15 @@ export function initBurgerMenu() {
     if (burgerMenu.classList.contains('burger-menu-show')) {
       burgerToggle.innerHTML = `
         <svg width="18" height="18" stroke="#000">
-          <use href="../img/icons.svg#icon-close"></use>
+          <use href="${iconClose}"></use>
         </svg>`;
       header.classList.add('overflow-hidden');
-      document.body.classList.add('burger-menu-show');
     } else {
       burgerToggle.innerHTML = `
         <svg width="28" height="28">
-          <use href="../img/icons.svg#icon-menu"></use>
+          <use href="${iconMenu}"></use>
         </svg>`;
       header.classList.remove('overflow-hidden');
-      document.body.classList.remove('burger-menu-show');
     }
   });
 }
