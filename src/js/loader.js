280368cee8c8dedbf55c5050e8ee1b28d6d53d35
Liubov-iam <liuboviam16@gmail.com>
2024-03-06 11:08:14 +0200

    лоадер

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,4 +1,9 @@
-// Добавить в html <span class="loader"></span>
+import { BookApi } from './book-api';
+
+const bookApi = new BookApi();
+const categoriesList = document.querySelector('.sidebar-categories-name');
+const scrollingLine = document.querySelector('.scrolling-line');
+const booksWrap = document.querySelector('.books-wrapper');
 const loader = document.querySelector('.loader');
 
 export function showLoader() {
@@ -8,3 +13,29 @@ export function showLoader() {
 export function closeLoader() {
   loader.classList.add('hidden');
 }
+
+async function renderCategories() {
+  try {
+    showLoader(); 
+
+    const categoryList = await bookApi.getCategoryList();
+
+    categoriesList.innerHTML = categoryList
+      .map(category => templateNameCategory(category))
+      .join('');
+
+    hideLoader(); 
+  } catch (error) {
+    hideLoader(); 
+    console.error('Error fetching category list:', error.message);
+  }
+}
+
+function templateNameCategory({ list_name: category }) {
+  return `<li class="sb-category-item">
+            <a href="#" data-category="${category}">${category}</a>
+          </li>`;
+}
+
+
+renderCategories();
