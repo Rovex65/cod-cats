7e9e281e10e0fe959e31e6e5fb9166f464bb3eca
Rovex65 <rovex65@gmail.com>
2024-03-08 16:14:48 +0200

    add fonts and adaptive 320px

--- src/js/modal-menu.js
+++ src/js/modal-menu.js
@@ -1,32 +1,29 @@
-import iconClose from '../img/icons.svg#icon-close'
-import iconMenu from '../img/icons.svg#icon-menu'
+import iconClose from '../img/icons.svg#icon-close';
+import iconMenu from '../img/icons.svg#icon-menu';
 
-export function initBurgerMenu() {
-  const burgerToggle = document.getElementById('burgerToggle');
-  const burgerMenu = document.getElementById('burgerMenu');
-  const header = document.querySelector('header');
+const burgerToggle = document.getElementById('burgerToggle');
+const burgerMenu = document.getElementById('burgerMenu');
+const header = document.querySelector('header');
+const body = document.querySelector('body');
 
-  burgerToggle.addEventListener('click', function () {
-    burgerMenu.classList.toggle('burger-menu-show');
+burgerToggle.addEventListener('click', function () {
+  burgerMenu.classList.toggle('burger-menu-show');
 
-    if (burgerMenu.classList.contains('burger-menu-show')) {
-      burgerToggle.innerHTML = `
+  if (burgerMenu.classList.contains('burger-menu-show')) {
+    burgerToggle.innerHTML = `
         <svg width="18" height="18" stroke="#000">
           <use href="${iconClose}"></use>
         </svg>`;
-      header.classList.add('overflow-hidden');
-    } else {
-      burgerToggle.innerHTML = `
+    body.classList.add('modal-is-open');
+    window.addEventListener('resize', checkNoScrollBody);
+  } else {
+    burgerToggle.innerHTML = `
         <svg width="28" height="28">
           <use href="${iconMenu}"></use>
         </svg>`;
-      header.classList.remove('overflow-hidden');
-    }
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  initBurgerMenu();
+    body.classList.remove('modal-is-open');
+    window.removeEventListener('resize', checkNoScrollBody);
+  }
 });
 
 const activePage = window.location.pathname;
@@ -44,4 +41,16 @@ navLinks.forEach(link => {
 
 if (!foundActiveLink) {
   navLinks[0].classList.add('burger-menu-nav-link-active');
+}
+
+function checkNoScrollBody(e) {
+  console.log(window.innerWidth);
+  if (window.innerWidth >= 768) {
+    header.classList.remove('overflow-hidden');
+    body.classList.remove('modal-is-open');
+    burgerMenu.classList.toggle('burger-menu-show');
+    window.removeEventListener('resize', checkNoScrollBody);
+  } else {
+    body.classList.add('modal-is-open');
+  }
 }
